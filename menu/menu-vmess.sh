270b191a5fc6f0dd0c15aa281fec18423c903f46
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-27 17:37:37 +0800

    Update menu-vmess.sh

--- menu/menu-vmess.sh
+++ menu/menu-vmess.sh
@@ -114,6 +114,7 @@ echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Trial Account XRAY Vmess     "
 echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Extending Account XRAY Vmess Active "
 echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Delete Account XRAY Vmess Websocket  "
 echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Check User Login XRAY Vmess     "
+echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Check XRAY Vmess Config     "
 echo -e ""
 echo -e "     ${BICyan}[${BIWhite}00${BICyan}] Back to main menu     "
 
@@ -128,6 +129,7 @@ case $opt in
 3) clear ; renew-ws ; exit ;;
 4) clear ; del-ws ; exit ;;
 5) clear ; cek-ws ; exit ;;
+6) clear ; user-ws ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Wrong Button " ; sleep 1 ; menu-vmess ;;
