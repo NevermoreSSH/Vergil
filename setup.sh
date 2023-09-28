49eafd157a20bd7b1c87172e112a7f5fb09723b0
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-28 22:19:35 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -176,7 +176,7 @@ echo -e "$green          Install SLDNS              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/autoscript-ssh-slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/NevermoreSSH/Vergil/main2/addons/dns2.sh && chmod +x dns2.sh && ./dns2.sh
 clear
 
 #Instal udp
