2d3523c25fcd3cdc101bb60f711b594c62d855b8
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-28 12:22:17 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -132,8 +132,7 @@ clear;clear;clear
 
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
-echo -e "         OWNER : ${GREEN} BTR ${NC}${YELLOW}(${NC} ${GREEN} BIGETRO RED ALIEND  ${NC}${YELLOW})${NC}"
-echo -e "       © Copyright By premi vps ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
+echo -e "       © Copyright Premium Scripts ${YELLOW}(${NC} 2022-2023 ${YELLOW})${NC}"
 echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
 echo -e "${RED_BG}                     Sytem Information                    ${NC}"
