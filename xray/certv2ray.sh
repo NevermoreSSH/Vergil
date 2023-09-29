7621f6aaf0d0f6f94025105631d6b3b85e8d152b
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-29 10:53:01 +0800

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -41,5 +41,5 @@ systemctl restart nginx
 echo -e "[ ${green}INFO${NC} ] All finished... " 
 sleep 0.5
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+echo -e ""
+echo -e "Script Mod By NevermoreSSH"
