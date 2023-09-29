ee528e7515989829b5e28e41ca7a74d355940330
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-29 10:49:52 +0800

    Update ceklim.sh

--- ssh/ceklim.sh
+++ ssh/ceklim.sh
@@ -26,6 +26,5 @@ fi
 echo " ";
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " ";
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu-ssh
+echo -e ""
+echo -e "Script Mod By NevermoreSSH"
