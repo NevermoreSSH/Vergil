1bbde8d2c0dd4675b807e8b8823e94cdded7f91b
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-09-29 11:07:41 +0800

    Update trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -38,7 +38,5 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e "Expired On     : $exp"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
+echo -e ""
+echo -e "Script Mod By NevermoreSSH"
