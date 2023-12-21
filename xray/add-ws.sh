6b64d9ec0b4439a1e1e60167c4a0936f9b5a6873
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-22 07:25:45 +0800

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -105,7 +105,7 @@ cat > /home/vps/public_html/vless-$user.txt <<-END
              P R O J E C T  O F  N E V E R M O R E S S H
                        [Freedom Internet]
 ====================================================================
-             https://github.com/NevermoreSSH/
+             https://github.com/NevermoreSSH/Vergil
 ====================================================================
              Format Xray/Vmess Account WS - V2
 ====================================================================
